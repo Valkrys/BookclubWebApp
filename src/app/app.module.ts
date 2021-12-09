b82c8b1891c34fca00e599e1f1bb112220d193cf
Jana Latzberg <j.latzberg@outlook.com>
2021-12-09 13:22:28 +0000

    Add basic calendar view to homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,12 @@ import { HomepageComponent } from './homepage/homepage.component';
     HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { startOfDay } from 'date-fns';
+import { CalendarView, CalendarEvent } from 'angular-calendar';
 
 @Component({
   selector: 'app-homepage',
@@ -10,6 +12,30 @@ export class HomepageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  viewDate: Date = new Date();
+  view: CalendarView = CalendarView.Month;
+  CalendarView = CalendarView;
+
+  setView(view: CalendarView) {
+    this.view = view;
+  }
+
+  events: CalendarEvent[] = [
+    {
+      start: startOfDay(new Date()),
+      title: 'First event',
+    },
+    {
+      start: startOfDay(new Date()),
+      title: 'Second event',
+    }
+  ]
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    console.log(date);
+    //let x=this.adminService.dateFormat(date)
+    //this.openAppointmentList(x)
   }
 
 }
